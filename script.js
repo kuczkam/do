7fed70e3446635191960fe63a059df635b6ee7db
root <root@be.localdomain>
2018-08-08 20:37:21 +0200

    3

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-(function() {
+let tasks = (function(){
     'use strict';
-    let tasks = {
-        tasks: [],
-        addNewTask: function() {
-            this.addTask.push(this.tasks);
-            console.log(this.tasks);
+ 
+    const tasksList = [];
+    const input = document.getElementsByClassName('task-input');
+    const button = document.getElementsByClassName('task-input__button');
+ 
+    return {
+        getTasks () {
+          return tasksList
+        },
+        addTask (name) {
+          tasksList.push(name);
+  
+          return this;
         }
     }
-})();
+  })();
+ 
+  tasks.addTask('dupa')
+  console.log(tasks.addTask('test').getTasks())
