b4b71897931c74aaae8dda2e5a822806f46e1f24
kuczkam <42166328+kuczkam@users.noreply.github.com>
2018-09-04 13:22:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 let tasks = (function(){
-    'use strict';
-  
-    const tasksList = [];
-    const input = document.getElementById('task-input');
-    const list = document.getElementById('tasks');
+  'use strict';
+
+  const tasksList = [];
+  const input = document.getElementById('task-input');
+  const list = document.getElementById('tasks');
+
+  const addTask = function() {
+    if (input.value != '') {
+      tasksList.push(input.value);
+    }
+    taskList();
+    input.value = '';
+  }
+
+  const bindEvents = function() {
+    document.getElementById('task-input__button').addEventListener('click', addTask);
+    list.addEventListener('click', deleteTask);
+  }
+
+  const taskList = function() {
     const frag = document.createDocumentFragment();
+    list.innerHTML = '';
 
-    const addTask = function() {
-      if (input.value != '') {
-        tasksList.push(input.value);
-      }
-      taskList();
-      input.value = '';
+    tasksList.forEach(function(el) {
+      const element = document.createElement('li');
+      const remove = document.createElement('button');
+      const rm = document.createTextNode('remove');
+
+      element.setAttribute('class', 'todo-element');
+      remove.setAttribute('id', 'task-remove__button');
+      remove.appendChild(rm);
+      element.innerHTML = el;
+      element.appendChild(remove);
+      frag.appendChild(element);
+      list.appendChild(frag);
+    });
+}
+
+  const deleteTask = function(e) {
+    if (e.target.closest('#task-remove__button') !== null) {
+      const todoElem = e.target.closest('.todo-element');
+      todoElem.parentNode.removeChild(todoElem);
+      tasksList.splice(todoElem, 1);
     }
-
-    const bindEvents = function() {
-      document.getElementById('task-input__button').addEventListener('click', addTask);
-      list.addEventListener('click', deleteTask);
-    }
-
-    const taskList = function() {
-      list.innerHTML = '';
-
-      tasksList.forEach(function(el) {
-        let element = document.createElement('li');
-        let remove = document.createElement('button');
-        let rm = document.createTextNode('remove');
-
-        element.setAttribute('class', 'todo-element');
-        remove.setAttribute('id', 'task-remove__button');
-        remove.appendChild(rm);
-        element.innerHTML = el;
-        element.appendChild(remove);
-        frag.appendChild(element);
-        list.appendChild(frag);
-      });
   }
 
-    const deleteTask = function(e) {
-      if (e.target.closest('#task-remove__button') !== null) {
-        const todoElem = e.target.closest('.todo-element');
-        todoElem.parentNode.removeChild(todoElem);
-        tasksList.splice(todoElem, 1);
-      }
-    }
-
-    const init = function() {
-      bindEvents();
-      taskList();
+  const init = function() {
+    bindEvents();
+    taskList();
+}
+  return {
+      init: init,
+      addTask: addTask,
+      taskList: taskList,
+      deleteTask: deleteTask
   }
-    return {
-        init: init,
-        addTask: addTask,
-        taskList: taskList,
-        deleteTask: deleteTask
-    }
-  })();
-  tasks.init();
+})();
+tasks.init();
