6eea08659f8ffece42a9646db7e6710361c6f9fd
Kamil <b-l-u-e-e-y-e-s@github.com>
2018-10-31 20:36:27 +0100

    date

--- datepicker.js
+++ datepicker.js
@@ -1,7 +1,7 @@
 let datepicker = (() => {
     'use strict'
     const month = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
-    const td = document.getElementById('js__days');
+    const tbody = document.getElementById('js__days');
     const frag = document.createDocumentFragment();
     let d = new Date();
     let m = d.getMonth();
@@ -11,16 +11,28 @@ let datepicker = (() => {
         let short_name = month[m];
         let days = __getDayOfTheMonth(m, y);
         
-        for (let i = 1; i <= days; i++) {
-            const td = document.createElement('td');
-            td.innerHTML = i;
-            frag.appendChild(td);
-        }
+        // for (let i = 1; i <= days; i++) {
+        //     const td = document.createElement('td');
+        //     td.innerHTML = i;
+        //     frag.appendChild(td);
+        // }
 
-        td.appendChild(frag);
+        // td.appendChild(frag);
         
-        console.log(__getDayOfTheMonth(m, y));
-        console.log(short_name);
+        // console.log(__getDayOfTheMonth(m, y));
+        // console.log(short_name);
+
+    //     for ( let i = 0; i <= 4; i++ ) {
+    //         const tr = document.createElement('TR');
+    //         tbody.appendChild(tr);
+    //         for ( let j = 0; j <= 7; j++ ) {
+    //             const td = document.createElement('TD');
+    //             for ( let x = 1; x <= days; x++ ) {
+    //                 td.innerHTML = x;
+    //                 tr.appendChild(td);
+    //             }
+    //         }
+    //     }
     }
 
     const __getDayOfTheMonth = (m, y) => {
